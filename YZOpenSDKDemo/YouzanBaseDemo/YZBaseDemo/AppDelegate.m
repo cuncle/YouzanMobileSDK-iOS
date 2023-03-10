68477d43f5788edcb24d6f5f07978cc0cbe6d49e
cuncle <uncle5@outlook.com>
2023-03-10 17:18:34 +0800

    Update AppDelegate.m

--- YZOpenSDKDemo/YouzanBaseDemo/YZBaseDemo/AppDelegate.m
+++ YZOpenSDKDemo/YouzanBaseDemo/YZBaseDemo/AppDelegate.m
@@ -20,12 +20,13 @@ static NSString *const SCHEME = @"yzbasedemo";/**< demo 的 scheme */
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     
+    YZSDK.shared.delegate = self; // 必须设置代理方法
     // 初始化sdk
-    YZSDK.shared.delegate = self; // 必须设置代理方法，保证 SDK 在需要 token 的时候可以正常运行
     YZConfig *config = [[YZConfig alloc] initWithClientId:CLIENT_ID andAppKey:APPKEY];
     config.enableLog = NO; // 关闭 sdk 的 log 输出
     config.scheme = SCHEME; // 配置 scheme 以便微信支付完成后跳转
     [YZSDK.shared initializeSDKWithConfig:config];
+
     
     // 查看 sdk 的版本
     NSLog(@"%@", YZSDK.shared.version);
